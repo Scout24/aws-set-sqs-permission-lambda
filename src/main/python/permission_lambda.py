085436ba30d00f0300f05db29784411da0df3984
Tobias Vollmer <tobias.vollmer@immobilienscout24.de>
2015-12-01 18:32:26 +0100

    PAIO-129: Add source

--- src/main/python/permission_lambda.py
+++ src/main/python/permission_lambda.py
@@ -1,6 +1,12 @@
 from __future__ import print_function, division, absolute_import
+from botocore.exceptions import ClientError
 import boto3
 import json
+
+
+class AlreadyExistsException(Exception):
+    """Exception class for throwing if Element already exists"""
+    pass
 
 
 def get_usofa_accountlist(bucketname):
@@ -13,8 +19,8 @@ def _get_usofa_data(bucketname):
     """ Get USofA data from Bucket
     Return dict
     """
-    conn = boto3.client('s3')
-    result = conn.get_object(Bucket=bucketname, Key='accounts.json')
+    client = boto3.client('s3')
+    result = client.get_object(Bucket=bucketname, Key='accounts.json')
     return json.loads(result['Body'].read().decode('utf-8'))
 
 
@@ -22,15 +28,27 @@ def delete_permissions(queue_url, label):
     """ Delete Permissions by label (Sid)
     Return: None
     """
-    pass
+    client = boto3.client('sqs')
+    try:
+        client.remove_permission(QueueUrl=queue_url, Label=label)
+    except ClientError as e:
+        if e.response['Error']['Code'] != 'InvalidParameterValue':
+            raise
+        # We are failing silently since the label, which we would like to delete, does not exist
 
 
 def set_permissions(queue_url, label, permissionlist, accountidlist):
     """ Set given Permissions for all accounts of accountlist.
     Return: None
     """
-    pass
-
+    client = boto3.client('sqs')
+    try:
+        client.add_permission(QueueUrl=queue_url, Label=label,
+                              AWSAccountIds=accountidlist, Actions=permissionlist)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'InvalidParameterValue':
+            raise AlreadyExistsException(e.response['Error']['Message'])
+        raise
 
 def update_sqs_permissions(event, context):
     """ Institute permission for all accountids in the USofA on sqs queue
